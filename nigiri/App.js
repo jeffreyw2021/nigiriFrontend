19e9248a6c7d9de432fdb3f957722e1fb6eab23c
jeffrey <jeffrey.w@wustl.edu>
2023-12-14 20:42:03 -0600

    updated with async timer and notification

--- nigiri/App.js
+++ nigiri/App.js
@@ -1,14 +1,12 @@
-//eas build --platform ios
-//eas submit -p ios --latest
-import registerNNPushToken from 'native-notify';
 import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './app/components/AppNavigator';
 import { View, ActivityIndicator } from 'react-native';
 
-export default function App(){
-  registerNNPushToken(16776, 'vip8IrtRHBkbyLFDjVhRhR');
+const App = () => {
   return (
     <AppNavigator />
   );
 };
+
+export default App;
